18447c2bc45cb4d71e638f8c2fd6973648419f1c
Adrielle <adriellesantosalves@hotmail.com>
2021-09-26 17:06:25 -0300

    feat: change folder names to lowercase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Starwars } from '../Assets/starwars.svg';
+import { ReactComponent as Starwars } from '../assets/starwars.svg';
 
 const Header = () => {
   return (
